56d3442f7d8045ba4f86cd8f69a3f0824c845ace
Joseph Anger <jsphanger@yahoo.com>
2020-04-06 18:12:10 -0500

    (Bug Fix) Removed the inner UIView.
    - Removed the inner UIView 
    - Set own properties instead

--- PopUpPlayer.iOS/NativeControls/AudioPlayer.cs
+++ PopUpPlayer.iOS/NativeControls/AudioPlayer.cs
@@ -7,10 +7,6 @@ namespace PopUpPlayer.iOS.CustomRenderers
 {
     public class AudioPlayer : UIView
     {
-        public override CGRect Frame { get => base.Frame; set => base.Frame = value; }
-
-        public UIView PlayerView { get; set; }
-
         //-- Items needed for small view
         public UIImageView PreviewImage { get; set; }
         public UIImageView PlayPauseButton { get; set; }
@@ -44,40 +40,36 @@ namespace PopUpPlayer.iOS.CustomRenderers
         public void DisplayMiniPlayerView()
         {
             //Define our mini player view
-            PlayerView = new UIView(new CGRect(0, 0, this.Frame.Width, this.Frame.Height));
-            PlayerView.Layer.BorderWidth = 1;
-            PlayerView.Layer.BorderColor = UIColor.DarkGray.CGColor;
-            PlayerView.BackgroundColor = UIColor.LightGray;
+            Layer.BorderWidth = 1;
+            Layer.BorderColor = UIColor.DarkGray.CGColor;
+            BackgroundColor = UIColor.LightGray;
 
             UITapGestureRecognizer playerTappedRecognizer = new UITapGestureRecognizer(PlayerTapped);
-            PlayerView.AddGestureRecognizer(playerTappedRecognizer);
-            PlayerView.UserInteractionEnabled = true;
+            AddGestureRecognizer(playerTappedRecognizer);
+            UserInteractionEnabled = true;
 
             //Create a play button
             var image = new UIImage("xamarin_logo.png");
             PlayPauseButton = new UIImageView(image);
-            PlayPauseButton.Frame = new CGRect(PlayerView.Bounds.X + 20, PlayerView.Bounds.Y, 50, 50);
+            PlayPauseButton.Frame = new CGRect(Bounds.X + 20, Bounds.Y, 50, 50);
 
             UITapGestureRecognizer playTapRecognizer = new UITapGestureRecognizer(PlayButtonTapped);
             PlayPauseButton.AddGestureRecognizer(playTapRecognizer);
             PlayPauseButton.UserInteractionEnabled = true;
 
-            PlayerView.AddSubview(PlayPauseButton);
-
-            //Add our mini view and playbutton
-            AddSubview(PlayerView);
+            AddSubview(PlayPauseButton);
         }
 
         public void UpdatePlayerView()
         {
-            UIView.Animate(1, () => {
+            Animate(1, () => {
 
                 var heightChange = 50;
 
                 if (!IsLargeView)
-                    PlayerView.Frame = new CGRect(PlayerView.Frame.X, PlayerView.Frame.Y - heightChange, PlayerView.Frame.Width, PlayerView.Frame.Height + heightChange);
+                    Frame = new CGRect(Frame.X, Frame.Y - heightChange, Frame.Width, Frame.Height + heightChange);
                 else
-                    PlayerView.Frame = new CGRect(PlayerView.Frame.X, PlayerView.Frame.Y + heightChange, PlayerView.Frame.Width, PlayerView.Frame.Height - heightChange);
+                    Frame = new CGRect(Frame.X, Frame.Y + heightChange, Frame.Width, Frame.Height - heightChange);
 
                 IsLargeView = !IsLargeView;
             });
